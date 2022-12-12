f8e8d3caf9bb57f59cb7c9ff44204dfe229250d0
MICHAEL NAM (DSENG-ISD-OOCLL/HKG) <NAMMI@OOCL.COM>
2022-12-12 19:20:12 +0800

    fix: handleNewItemChange

--- src/features/TodoGenerator.js
+++ src/features/TodoGenerator.js
@@ -2,9 +2,13 @@ import React from "react";
 
 export default function TodoGenerator(props) {
   const { newItem, onNewItemChange, onNewItemAdd } = props;
+
+  const handleNewItemChange = (event) =>{
+    onNewItemChange(event.target.value);
+  }
   return (
     <div>
-      <input type="text" value={newItem} onChange={onNewItemChange} />
+      <input type="text" value={newItem} onChange={handleNewItemChange} />
       <button
         type="button"
         style={{ backgroundColor: "blue", color: "white" }}
